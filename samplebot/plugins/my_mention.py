2b5a6e340b537fdf523e896e0ac59f35ea0df0d4
KamataHiromichi <1216kamata@gmail.com>
2018-04-10 13:03:36 +0900

    完成！

--- samplebot/plugins/my_mention.py
+++ samplebot/plugins/my_mention.py
@@ -1,43 +1,16 @@
 # coding: utf-8
 
-from slackbot.bot import default_reply  #該当する応答がない場合に反応するデコーダ
+from slackbot.bot import default_reply
 import pylab as plt
 import numpy as np
-import requests
-from slackclient import SlackClient
 
-# @respond_to('string')     bot宛のメッセージ
-#                           stringは正規表現が可能 「r'string'」
-# @listen_to('string')      チャンネル内のbot宛以外の投稿
-#                           @botname: では反応しないことに注意
-#                           他の人へのメンションでは反応する
-#                           正規表現可能
-# @default_reply()          DEFAULT_REPLY と同じ働き
-#                           正規表現を指定すると、他のデコーダにヒットせず、
-#                           正規表現にマッチするときに反応
-#                           ・・・なのだが、正規表現を指定するとエラーになる？
-
-# message.reply('string')   @発言者名: string でメッセージを送信
-# message.send('string')    string を送信
-# message.react('icon_emoji')  発言者のメッセージにリアクション(スタンプ)する
-#                               文字列中に':'はいらない
 @default_reply()
 def default(message):
-    message.reply("デフォルトの返答")
-
     text = message.body['text']
     try:
         pl_equation(text, outfile='output', padding=0.1)
         image_pass = 'output.png'
-        # files = {'image': open(image_pass, 'rb')}
-        # param = {"title": "output image", 'token': "xoxb-342789435984-HmOb1nseXolLo2VLaRUIuREs", 'channels': "C7EDDH883",}
-        # requests.post(url="https://slack.com/api/files.upload", params=param, files=files)
-
-        access_token = "xoxb-342789435984-HmOb1nseXolLo2VLaRUIuREs"
-        CHANNEL_ID = "CA37HHE2G"
-        with open(image_pass, 'rb') as f:
-            param = {'token': access_token, 'channels': CHANNEL_ID, 'title': 'output_image'}
-            r = requests.post("https://slack.com/api/files.upload", params=param, files={'file': f})
+        message.channel.upload_file(fname='output', fpath=image_pass)
     except:
         message.reply("画像に変換できません")
 
